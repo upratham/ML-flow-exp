53056e7b40c954af22483b63cb3158fd9356793e
PRATHAMESH SUHAS URAVANE <upratham2002@gmail.com>
2025-11-26 17:28:06 -0500

    Upto logging accuracy

---
+++ src/file1.py
@@ -0,0 +1,35 @@
+import mlflow
+from sklearn.datasets import load_wine
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,accuracy_score
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+#import wine data
+
+wine=load_wine()
+X=wine.data
+y=wine.target
+
+#Train text split 
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.1,random_state=21)
+
+# Hyperparameters
+
+max_depth=5
+n_estimators=100
+
+with mlflow.start_run():
+    rfc=RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators,random_state=1)
+    rfc.fit(X_train,y_train)
+    y_pred= rfc.predict(X_test)
+    accuracy= accuracy_score(y_test,y_pred)
+    
+    mlflow.log_metric('Accuracy', accuracy)
+    mlflow.log_param('max_depth',max_depth)
+    mlflow.log_param('n_estimators',n_estimators)
+
+    print('acuuracy:',accuracy)
